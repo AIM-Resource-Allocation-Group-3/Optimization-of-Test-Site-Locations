924028b83c8eb0b2d9b93715baac090862727aef
Sabrina Hetzel <shetzel@smu.edu>
2020-07-20 18:42:38 -0500

    Working nonlinear objective function

--- nonlinear_opt_site_graphs.m
+++ nonlinear_opt_site_graphs.m
@@ -1,5 +1,5 @@
 % Very slow and puts testing sites on boundaries
-function[x,fval,exitFlag,Output] = nonlinear_opt_site_graphs(m,n,currentI,neib,dneib)
+function[x,fval,exitFlag,Output,pop,scores] = nonlinear_opt_site_graphs(m,n,currentI,neib,dneib)
 I = currentI; %number of infected individuals at each node
 
 C = 1; %number of pop-up testing sites
@@ -28,10 +28,10 @@ fun = @(F) objfun(m,n,I,M,R,G,F,neib,dneib);
     % PlotFcn - displays plot as the optimization is working
     % MaxStallGenerations - one possible stopping criteria (looks at avg relative change)
     % MaxGerations - number of iterations before ga stops
-options = optimoptions('ga','PopulationSize',500,'PlotFcn',{@gaplotbestf,@gaplotstopping},...
-    'MaxStallGenerations', 300,'MaxGenerations',200);
+options = optimoptions('ga','PopulationSize',200,'PlotFcn',{@gaplotbestf,@gaplotstopping},...
+    'FunctionTolerance',10e-3,'MaxStallGenerations',15,'MaxGenerations',20);
 options = optimoptions(options,'UseVectorized',true);
-[x,fval,exitFlag,Output] = ga(fun,m*n,A,b,Aeq,beq,lb,ub,nonlcon,intcon,options);
+[x,fval,exitFlag,Output,pop,scores] = ga(fun,m*n,A,b,Aeq,beq,lb,ub,nonlcon,intcon,options);
 end
 
 function L = objfun(m,n,I,M,R,G,F,neib,dneib)
@@ -42,5 +42,5 @@ function L = objfun(m,n,I,M,R,G,F,neib,dneib)
         L(node) = I(node)*(1 - M(1)*R(node) - G(1)*F(node) - M(2)*sum(R(nnode)) ...
             - G(2)*sum(F(nnode)) - M(3)*sum(R(dnode)) - G(3)*sum(F(dnode)));
     end
-    L = -sum(F*L);
+    L = -F*L;
 end
