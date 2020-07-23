df201ab535303ac453a2ad5c74ce4a018011b257
Sabrina Hetzel <shetzel@smu.edu>
2020-07-23 14:52:45 -0500

    Looping over possible test site locations instead of all nodes

--- exploreShapeFiles.m
+++ exploreShapeFiles.m
@@ -3,13 +3,13 @@
 % A - POI of NH (represented as a point)
 % B - Census Tract of NH (polygons)
 
-S = shaperead('NH_Population_Density-shp/USA_Population_Density.shp');
+S = shaperead('Data/NH_Population_Density-shp/USA_Population_Density.shp');
 % A = shaperead('new_hampshire_point_of_interest/new_hampshire_poi.shp');
-B = shaperead('tl_2016_33_tract/tl_2016_33_tract.shp');
+B = shaperead('Data/tl_2016_33_tract/tl_2016_33_tract.shp');
 
-Hospitals = shaperead('New_Hampshire_Hospitals-shp/9c19a9cd-6911-4892-8c7b-ea3639c50f76202049-1-j3z5s6.y65y.shp');
-PlacesOfWorship = shaperead('New_Hampshire_Places_of_Worship-shp/New_Hampshire_Places_of_Worship.shp');
-Pop = shaperead('tl_2019_33_bg/tl_2019_33_bg.shp');
+Hospitals = shaperead('Data/New_Hampshire_Hospitals-shp/9c19a9cd-6911-4892-8c7b-ea3639c50f76202049-1-j3z5s6.y65y.shp');
+PlacesOfWorship = shaperead('Data/New_Hampshire_Places_of_Worship-shp/New_Hampshire_Places_of_Worship.shp');
+Pop = shaperead('Data/tl_2019_33_bg/tl_2019_33_bg.shp');
 
 Cxworship = zeros(size(struct2cell(PlacesOfWorship),2),1);
 Cyworship = zeros(size(struct2cell(PlacesOfWorship),2),1);
@@ -29,16 +29,36 @@ hold on
 plot(Cxworship,Cyworship,'.b','MarkerSize',10)
 plot(Cxhosp,Cyhosp,'.r','MarkerSize',10)
 
-numTracts = struct2cell(Pop);
+numTracts = struct2cell(S);
 Cx = zeros(size(numTracts,2),1); Cy = zeros(size(numTracts,2),1);
 polgon = polyshape();
 for i = 1:size(numTracts,2)
-    polgon(i) = polyshape(Pop(i).X,Pop(i).Y);
+    polgon(i) = polyshape(S(i).X,S(i).Y);
     [Cx(i),Cy(i)] = centroid(polgon(i));
 end
 plot(Cx,Cy,'.g','MarkerSize',10)
 hold off
 
+%% dsearchn - find nearest hospital from centroid of county
+PQ = []; % query points
+for i = 1:length(Cx)
+    PQ = [PQ;Cx(i), Cy(i)];
+end
+F = []; %fixed data points
+for i = 1:length(Cxhosp)
+    F = [F;Cxhosp(i), Cyhosp(i)];    
+end
+[k,dis] = dsearchn(F,PQ);
+
+plot(F(:,1),F(:,2),'ko')
+hold on
+plot(PQ(:,1),PQ(:,2),'*g')
+hold on
+plot(F(k,1),F(k,2),'*r')
+legend('Data Points','Query Points','Nearest Points','Location','sw')
+
+% rangesearch
+[Idx,D] = rangesearch(F,PQ,0.5)
 %%
 [X,Y] = boundingbox(polgon);
 [Adj,obj] = make_graph(6,6,X(1),Y(1),X(2),Y(2));
--- nonlinear_opt_site_graphs.m
+++ nonlinear_opt_site_graphs.m
@@ -1,9 +1,9 @@
 % Very slow and puts testing sites on boundaries
-function[x,fval,exitFlag,Output] = nonlinear_opt_site_graphs(currentI,didt,neib,dneib,tau,possTest)
+function[x,fval,exitFlag,Output] = nonlinear_opt_site_graphs(currentI,didt,neib,dneib,possTest,dist,fixneib,fixdneib,possneib,possdneib)
 I = currentI; %number of infected individuals at each node
 disp(length(I))
 C = 1; %number of pop-up testing sites
-R = zeros(length(I),1); %number of fixed testing sites at each node
+% R = zeros(length(I),1); %number of fixed testing sites at each node
 % fixed testing location modifier
 syms M(b)
 M(b) = piecewise(b==1,0.2,b==2,0.1,b==3,0.05);
@@ -16,16 +16,16 @@ G = [G(1), G(2), G(3)];
 
 Aeq = [];
 beq = [];
-A = ones(1,length(I));
+A = ones(1,length(possTest));
 b = C;
-lb = zeros(length(I),1);
-ub = zeros(length(I),1);
-ub(possTest) = 1;
+lb = zeros(length(possTest),1);
+ub = ones(length(possTest),1);
+% ub(possTest) = 1;
 nonlcon = [];
-intcon = 1:length(I);
-initPop = zeros(100,length(I));
+intcon = 1:length(possTest);
+initPop = zeros(100,length(possTest));
 
-fun = @(F) objfun(I,M,R,G,F,didt,neib,dneib,tau);
+fun = @(F) objfun(I,M,G,F,didt,neib,dneib,possTest,dist,fixneib,fixdneib,possneib,possdneib);
 % options for ga() solver (go to Input->options for full list): https://www.mathworks.com/help/gads/ga.html
     % PlotFcn - displays plot as the optimization is working
     % MaxStallGenerations - one possible stopping criteria (looks at avg relative change)
@@ -33,18 +33,24 @@ fun = @(F) objfun(I,M,R,G,F,didt,neib,dneib,tau);
 options = optimoptions('ga','PopulationSize',200,'PlotFcn',{@gaplotbestf,@gaplotstopping},...
     'FunctionTolerance',10e-3,'MaxStallGenerations',15,'MaxGenerations',20,'InitialPopulationMatrix',initPop);
 options = optimoptions(options,'UseVectorized',true);
-[x,fval,exitFlag,Output] = ga(fun,length(I),A,b,Aeq,beq,lb,ub,nonlcon,intcon,options);
+[x,fval,exitFlag,Output] = ga(fun,length(possTest),A,b,Aeq,beq,lb,ub,nonlcon,intcon,options);
 end
 
-function L = objfun(I,M,R,G,F,didt,neib,dneib,tau)
-    L = zeros(length(I),1); 
+function L = objfun(I,M,G,F,didt,neib,dneib,possTest,dist,fixneib,fixdneib,possneib,possdneib)
+    L = zeros(length(possTest),1); 
     disp(size(F))
-    for node = 1:length(L)
+    for node = 1:length(possTest)
         nnode = neib(node,:); nnode = nnode(nnode~=0);
         dnode = dneib(node,:); dnode = dnode(dnode~=0);
-        L(node) = ((0.5.*didt(node)'*I(node)) + (tau*0.5.*didt(nnode)'*I(nnode)) + ((tau^2)*0.5.*didt(dnode)'*I(dnode)))*...
-            (1 - M(1)*R(node) - G(1)*F(node) - M(2)*sum(R(nnode)) ...
-            - G(2)*sum(F(nnode)) - M(3)*sum(R(dnode)) - G(3)*sum(F(dnode)));
+        fixn = fixneib(node,:); fixn = fixn(fixn~=0);
+        fixd = fixdneib(node,:); fixd = fixd(fixd~=0);
+        possn = possneib(node,:); possn = possn(possn~=0);
+        possd = possdneib(node,:); possd = possd(possd~=0);
+%         L(node) = ((0.5.*didt(node)'*I(node)) + (tau*0.5.*didt(nnode)'*I(nnode)) + ((tau^2)*0.5.*didt(dnode)'*I(dnode)))*...
+%             (1 - M(1)*R(node) - G(1)*F(node) - M(2)*sum(R(nnode)) ...
+%             - G(2)*sum(F(nnode)) - M(3)*sum(R(dnode)) - G(3)*sum(F(dnode)));
+        L(node) = ((didt(nnode)'*I(nnode))/dist + (didt(dnode)'*I(dnode))/(2*dist))*...
+            (1 - M(1)*0 - G(1)*F(node) - M(2)*length(fixn) - G(2)*length(possn) - M(3)*length(fixd) - G(3)*length(possd));
     end
     L = -F*L;
 end
