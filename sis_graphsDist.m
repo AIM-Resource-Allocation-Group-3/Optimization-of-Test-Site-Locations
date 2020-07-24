2a7bd9da14f39a9b1211231cd5319c1c7325e050
jbe2 <jbe2@rice.edu>
2020-07-23 23:39:56 -0400

    added infrastructure for 3 compartment model SIQ

--- sis_graphsDist.m
+++ sis_graphsDist.m
@@ -29,6 +29,7 @@ disp(H.Nodes)
 S_tot = sum(H.Nodes.Suceptible);
 I_tot = sum(H.Nodes.Infected);
 
+%FOR POSSIBLE TEST SITES
 % Find nearest pop node within dist radius (neib)
 % Find pop nodes within 2*dist radius (dneib)
 % Find nearest fixed test site (fixneib)
@@ -39,7 +40,7 @@ for node = 1:length(possTest)
     ntemp = nearest(H,possTest(node),dist); dtemp = nearest(H,possTest(node),2*dist);
     fixtemp = intersect(ntemp,fixedTest); fixdtemp = intersect(dtemp,fixedTest);
     posstemp = intersect(ntemp,possTest); possdtemp = intersect(dtemp,possTest);
-    ntemp = ntemp(find(H.Nodes.Population(ntemp)));
+    ntemp = ntemp(find(H.Nodes.Population(ntemp))); %maps nearest nodes to nearest pop nodes
     fixneib(node,1:length(fixtemp)) = fixtemp; possneib(node,1:length(posstemp)) = posstemp;
     dtemp = dtemp(find(H.Nodes.Population(dtemp)));
     fixdneibdiff = setdiff(fixdtemp,fixtemp); possdneibdiff = setdiff(possdtemp,posstemp);
@@ -50,8 +51,31 @@ for node = 1:length(possTest)
     ntemp = []; dtemp = [];
 end
 
+%For POPULATIONS NODES
+%pop_fixneib nearest fixed test site <= dist
+%pop_fixdneib nearest fixed test sites  <=2dist
+%pop_possneib nearest possible test sites <= dist
+%pop_possdneib nearest possible test sites <= 2dist
+%pop_fixneib = zeros(length(popNodes));
+for node = 1:length(popNodes)   
+    ntemp = nearest(H,popNodes(node),dist); dtemp = nearest(H,popNodes(node),2*dist);
+    fixtemp = intersect(ntemp,fixedTest); fixdtemp = intersect(dtemp,fixedTest);
+    posstemp = intersect(ntemp,possTest); possdtemp = intersect(dtemp,possTest);
+    ntemp = ntemp(find(H.Nodes.Population(ntemp))); %maps nearest nodes to nearest pop nodes
+    pop_fixneib(node,1:length(fixtemp)) = fixtemp; pop_possneib(node,1:length(posstemp)) = posstemp;
+    dtemp = dtemp(find(H.Nodes.Population(dtemp)));
+    fixdneibdiff = setdiff(fixdtemp,fixtemp); possdneibdiff = setdiff(possdtemp,posstemp);
+    pop_fixdneib(node,1:length(fixdneibdiff)) = fixdneibdiff; pop_possdneib(node,1:length(possdneibdiff)) = possdneibdiff;
+%     neib(node,1:length(ntemp)) = ntemp;
+%     dtempDiff = setdiff(dtemp,ntemp);
+%     dneib(node,1:length(dtempDiff)) = dtempDiff;
+    ntemp = []; dtemp = [];
+end
+
+
 pop_possTest = union(popNodes',possTest);
 s = setdiff(1:m*n,pop_possTest);
+
 
 
 
@@ -63,6 +87,11 @@ nonlinx_tot = zeros(numsteps,1);
 I_neigh = zeros(m*n,1); S_neigh = zeros(m*n,1);
 didt = zeros(m*n,1); dsdt = zeros(m*n,1); Ival_ind = [];
 
+%New Model 
+dqdt = zeros(m*n,1)
+pop_ntempsite = zeros(size(H.Nodes.Infected));
+pop_dtempsite = zeros(size(H.Nodes.Infected));
+Quar_sto = zeros(size(H.Nodes.Infected),14)
 for t = 1:numsteps
     % Calculate Infected,Suceptible neighbors
     for i = 1:m*n
@@ -78,11 +107,24 @@ for t = 1:numsteps
     P = H.Nodes.Suceptible + H.Nodes.Infected + tau.*(I_neigh + S_neigh);
 
     %Solving the ODE
-    dsdt(popNodes) = -beta.*(H.Nodes.Infected(popNodes).*H.Nodes.Suceptible(popNodes))./P(popNodes) + gamma.*H.Nodes.Infected(popNodes) - tau.*(I_neigh(popNodes).*H.Nodes.Suceptible(popNodes))./P(popNodes);
-    didt(popNodes) = beta.*(H.Nodes.Infected(popNodes).*H.Nodes.Suceptible(popNodes))./P(popNodes) - gamma.*H.Nodes.Infected(popNodes) + tau.*(I_neigh(popNodes).*H.Nodes.Suceptible(popNodes))./P(popNodes);
+    dsdt(popNodes) = -beta.*(H.Nodes.Infected(popNodes).*H.Nodes.Suceptible(popNodes))./P(popNodes) + gamma.*H.Nodes.Infected(popNodes) - tau.*(I_neigh(popNodes).*H.Nodes.Suceptible(popNodes))./P(popNodes) + Quar_sto(:,:,i);
+    dqdt(popNodes) = H.Nodes.Infected(popNodes).*sum(logical(pop_fixneib),2)./dist + H.Nodes.Infected(popNodes).*sum(logical(pop_fixdneib),2)./(2*dist) + ...
+        H.Nodes.Infected(popNodes).*sum(pop_ntempsite,2)./dist + H.Nodes.Infected(popNodes).*sum(pop_dtempsite,2)./(2*dist);
+    didt(popNodes) = beta.*(H.Nodes.Infected(popNodes).*H.Nodes.Suceptible(popNodes))./P(popNodes) - gamma.*H.Nodes.Infected(popNodes) + tau.*(I_neigh(popNodes).*H.Nodes.Suceptible(popNodes))./P(popNodes) - dqdt(popNodes);
+   
+    %Explanation of dQdt
+        %multiplies number of infected at each node by number of fixed
+        %sites, and divides by radius. Logical converts pop_fixneib to a
+        %binary matrix and sum(...,2) adds along rows
+        
+        %no capacity, model assumes testing capacity <<< infected
+        %
     H.Nodes.Infected = H.Nodes.Infected + didt.*dt;
     H.Nodes.Suceptible = H.Nodes.Suceptible + dsdt.*dt;
-
+    H.Nodes.Quarentine = H.Nodes.Quarentine + dqdt.*dt
+    
+    
+    Quar_sto = [Quar_sto, H.Nodes.quarentine]
     I_tot = [I_tot, sum(H.Nodes.Infected)];
     S_tot = [S_tot, sum(H.Nodes.Suceptible)];
     
@@ -103,6 +145,19 @@ for t = 1:numsteps
     Ival_ind(t) = find(Ival == max(Ival)); 
 %     indI = find(H.Nodes.Infected == max(H.Nodes.Infected));
 %     I_indtot(t) = indI;
+
+
+%pop_ntempsite: popup sites <= dist from pop centers
+%pop_dtempsite: popup sites <= 2dist from popcenters
+for i = 1:length(popNodes)
+   ntemp = nearest(H,popNodes(node),dist); dtemp = nearest(H,popNodes(node),2*dist); 
+   ntempsite = intersect(ntemp,indnonlinx); dtempsite = intersect(dtemp,indnonlinx);
+   pop_ntempsite(node,1:length(ntempsite)) = ntempsite;
+   pop_dtempsite(node,1:length(dtempsite)) = dtempsite;
+   
+   
+end
+
     
 end
 
